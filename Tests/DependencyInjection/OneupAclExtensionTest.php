f043d9a833552102d93228aadf55f748301a5ada
Jim Schmid <jim.schmid@1up.io>
2013-07-28 16:26:57 +0200

    Added empty test suite directory/file structure.

---
+++ Tests/DependencyInjection/OneupAclExtensionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Oneup\AclBundle\Tests\DependencyInjection;
+
+class OneupAclExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIfTestSuiteLoads()
+    {
+        $this->assertTrue(true);
+    }
+}

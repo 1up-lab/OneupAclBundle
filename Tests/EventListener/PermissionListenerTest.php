0f8feeb2434ae48ef9fe0408b28ca1eb7bb7a04a
Jim Schmid <jim.schmid@1up.io>
2013-08-01 16:10:26 +0200

    Added a permission listener test that passes.

--- Tests/EventListener/PermissionListenerTest.php
+++ Tests/EventListener/PermissionListenerTest.php
@@ -3,6 +3,7 @@
 namespace Oneup\AclBundle\Tests\EventListener;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
 
 use Oneup\AclBundle\Annotation\AclCheck;
 use Oneup\AclBundle\EventListener\PermissionListener;
@@ -45,6 +46,40 @@ class PermissionListenerTest extends AbstractSecurityTest
 
         $checks = array(
             new AclCheck(array('value' => array('one' => 128)))
+        );
+
+        $request = new Request(array(), array(), array(
+            '_acl_permission' => $checks,
+            'one' => $object
+        ));
+
+        $event->expects($this->any())
+            ->method('getRequest')
+            ->will($this->returnValue($request))
+        ;
+
+        $this->listener->onKernelController($event);
+    }
+
+    public function testAccessGranted()
+    {
+        $object = new SomeObject(1);
+        $this->manager->addObjectPermission($object, $this->getToken(), MaskBuilder::MASK_VIEW);
+
+        $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\FilterControllerEvent')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $event->expects($this->any())
+            ->method('getController')
+            ->will($this->returnValue(array(
+                new TestController,
+                'indexAction'
+            )))
+        ;
+
+        $checks = array(
+            new AclCheck(array('value' => array('one' => 'VIEW')))
         );
 
         $request = new Request(array(), array(), array(

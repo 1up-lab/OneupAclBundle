44fc6f42bf8bd9b458b732fe1e11acecc0d9b4e2
Jim Schmid <jim.schmid@1up.io>
2013-08-01 14:48:30 +0200

    Adde some more annotation tests.

--- Tests/Annotation/DomainObjectTest.php
+++ Tests/Annotation/DomainObjectTest.php
@@ -37,6 +37,101 @@ class DomainObjectTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Oneup\AclBundle\Annotation\ClassPermissions', $classPermissions[0]);
     }
 
+    public function testRemoveKeyInAnnotation()
+    {
+        $root = new Acl\DomainObject();
+
+        $this->assertTrue($root->getRemove());
+    }
+
+    public function testDataOfAnnotationSimple()
+    {
+        $root = new Acl\DomainObject(array(
+            'remove' => false,
+            'value' => array(
+                new Acl\ClassPermissions(array(
+                    'value' => array(
+                        'ROLE_USER' => 1
+                    )
+                ))
+            )
+        ));
+
+        $this->assertFalse($root->getRemove());
+        $this->assertCount(1, $root->getClassPermissions());
+
+        foreach ($root->getClassPermissions() as $permission) {
+            $this->assertInstanceOf('Oneup\AclBundle\Annotation\ClassPermissions', $permission);
+            $this->assertTrue(is_array($permission->getRoles()));
+
+            $roles = $permission->getRoles();
+
+            foreach ($roles as $role) {
+                $this->assertTrue(is_array($roles));
+
+                $roleString = current(array_keys($roles));
+                $mask = current(array_values($roles));
+
+                $this->assertEquals($roleString, 'ROLE_USER');
+                $this->assertEquals($mask, 1);
+            }
+        }
+    }
+
+    public function testDataOfAnnotationComplex()
+    {
+        $root = new Acl\DomainObject(array(
+            'remove' => true,
+            'value' => array(
+                new Acl\ClassPermissions(array(
+                    'value' => array(
+                        'ROLE_USER' => 1,
+                        'ROLE_ADMIN' => 2,
+                    )
+                )),
+                new Acl\ClassPermissions(array(
+                    'value' => array(
+                        'ROLE_SUPER_ADMIN' => 4,
+                    )
+                ))
+            )
+        ));
+
+        $this->assertTrue($root->getRemove());
+        $this->assertCount(2, $root->getClassPermissions());
+
+        foreach ($root->getClassPermissions() as $permission) {
+            $this->assertInstanceOf('Oneup\AclBundle\Annotation\ClassPermissions', $permission);
+            $this->assertTrue(is_array($permission->getRoles()));
+
+            $roles = $permission->getRoles();
+
+            foreach ($roles as $role) {
+                $this->assertTrue(is_array($roles));
+
+                $roleString = current(array_keys($roles));
+                $mask = current(array_values($roles));
+
+                if ($roleString == 'ROLE_USER') {
+                    $this->assertEquals($mask, 1);
+                    continue;
+                }
+
+                if ($roleString == 'ROLE_ADMIN') {
+                    $this->assertEquals($mask, 2);
+                    continue;
+                }
+
+                if ($roleString == 'ROLE_SUPER_ADMIN') {
+                    $this->assertEquals($mask, 4);
+                    continue;
+                }
+
+                $this->fail();
+            }
+        }
+    }
+
     protected function getDomainObjectAnnotations()
     {
         $reader = new SimpleAnnotationReader();

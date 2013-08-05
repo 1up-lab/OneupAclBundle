e75120007d0ee513fdce8c21b885ea5787c93c5b
David Greminger <david.greminger@1up.io>
2013-08-05 09:32:03 +0200

    Added PermissionStrategyParameterIsSet test

--- Tests/DependencyInjection/OneupAclExtensionTest.php
+++ Tests/DependencyInjection/OneupAclExtensionTest.php
@@ -15,4 +15,13 @@ class OneupAclExtensionTest extends AbstractSecurityTest
     {
         $this->assertTrue(is_bool($this->container->getParameter('oneup_acl.remove_orphans')));
     }
+
+    public function testIfPermissionStrategyParameterIsSet()
+    {
+        $this->assertTrue(
+            'any' == $this->container->getParameter('oneup_acl.permission_strategy') ||
+            'all' == $this->container->getParameter('oneup_acl.permission_strategy') ||
+            'equal' == $this->container->getParameter('oneup_acl.permission_strategy')
+        );
+    }
 }

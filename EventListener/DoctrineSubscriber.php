e902c44bea61e44e123b6d5848213536df90b893
David Greminger <david.greminger@1up.io>
2013-08-08 10:35:38 +0200

    Fixes a unmonitored ServiceCircularReferenceException

--- EventListener/DoctrineSubscriber.php
+++ EventListener/DoctrineSubscriber.php
@@ -3,61 +3,59 @@
 namespace Oneup\AclBundle\EventListener;
 
 use Doctrine\Common\EventSubscriber;
-use Doctrine\Common\Annotations\Reader;
 use Doctrine\ORM\Event\LifecycleEventArgs;
-use Symfony\Component\Security\Acl\Permission\MaskBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
-use Oneup\AclBundle\Driver\DriverChain;
-use Oneup\AclBundle\Security\Acl\Model\AclManagerInterface;
 
 class DoctrineSubscriber implements EventSubscriber
 {
-    protected $chain;
-    protected $remove;
-    protected $manager;
+    protected $container;
 
-    public function __construct(DriverChain $chain, AclManagerInterface $manager, $remove)
+    public function __construct(ContainerInterface $container)
     {
-        $this->chain = $chain;
-        $this->manager = $manager;
-        $this->remove = $remove;
+        $this->container = $container;
     }
 
     public function postPersist(LifecycleEventArgs $args)
     {
+        $chain = $this->container->get('oneup_acl.driver_chain');
+        $manager = $this->container->get('oneup_acl.manager');
+
         $entity = $args->getEntity();
         $object = new \ReflectionClass($entity);
 
-        $metaData = $this->chain->readMetaData($object);
+        $metaData = $chain->readMetaData($object);
 
         if (!empty($metaData)) {
             // add class permissions
             foreach ($metaData['permissions'] as $permission) {
-                $this->manager->addClassPermission($entity, $permission[1], $permission[0]);
+                $manager->addClassPermission($entity, $permission[1], $permission[0]);
             }
 
             // add property permissions
             foreach ($metaData['properties'] as $name => $property) {
-                $this->manager->addClassFieldPermission($entity, $name, $property[1], $property[0]);
+                $manager->addClassFieldPermission($entity, $name, $property[1], $property[0]);
             }
         }
     }
 
     public function preRemove(LifecycleEventArgs $args)
     {
-        if (!$this->remove) {
+        $chain = $this->container->get('oneup_acl.driver_chain');
+        $manager = $this->container->get('oneup_acl.manager');
+        $remove = $this->container->getParameter('oneup_acl.remove_orphans');
+
+        if (!$remove) {
             return;
         }
 
         $entity = $args->getEntity();
         $object = new \ReflectionClass($entity);
 
-        $metaData = $this->chain->readMetaData($object);
+        $metaData = $chain->readMetaData($object);
 
-        if (!empty($metaData) && ($this->remove || !!$metaData['remove'])) {
-            $this->manager->revokeAllObjectPermissions($entity);
-            $this->manager->revokeAllObjectFieldPermissions($entity);
+        if (!empty($metaData) && ($remove || !!$metaData['remove'])) {
+            $manager->revokeAllObjectPermissions($entity);
+            $manager->revokeAllObjectFieldPermissions($entity);
         }
     }
 
